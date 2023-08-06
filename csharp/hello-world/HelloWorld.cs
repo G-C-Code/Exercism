49a10f10f3dc86522fef5326b2da5ac81cc3a7c9
GSC <gsc.code@gmail.com>
2023-08-06 06:03:48 +0200

    test

---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class HelloWorld
+{
+    public static void Main()
+    {
+        Console.WriteLine(Hello);
+    }
+    public static string Hello()
+    {
+        string hello = "Hello, World!";
+        return hello;
+    }
+}
