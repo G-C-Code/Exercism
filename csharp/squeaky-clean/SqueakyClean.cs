b9f67a00e3e1f6d9dfd26099634cbe4e7adc2bb0
BlockSC <79803546+BlockSC@users.noreply.github.com>
2023-08-09 13:04:14 +0200

    sync

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -8,7 +8,11 @@ public static class Identifier
     public static string Clean(string identifier)
     {
         string pattern = @"[\x00-\x1F\x7F]";
-        string modifiedIdentifier = Regex.Replace(identifier, pattern, "CTRL");
+        string modifiedIdentifier0 = Regex.Replace(identifier, pattern, "CTRL");
+
+        pattern = "[^a-zA-Z] -";
+        string modifiedIdentifier = Regex.Replace(modifiedIdentifier0, pattern, String.Empty);
+
         char[] charArray = modifiedIdentifier.ToCharArray();
 
         for (int i = 0; i < charArray.Length; i++)
@@ -19,6 +23,7 @@ public static class Identifier
             if (charArray[i] == '-' && i + 1 < charArray.Length)
             {
                 charArray[i + 1] = char.ToUpper(charArray[i + 1]);
+                charArray = new string(charArray).Remove(i,1).ToCharArray();
             }
         }
 
