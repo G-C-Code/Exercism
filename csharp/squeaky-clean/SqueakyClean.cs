2f9f0081b11a851776c5f24e5bd1e8c463e4b69f
GSC <gsc.code@gmail.com>
2023-08-09 12:26:58 +0200

    sync

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        string pattern = @"[\x00-\x1F\x7F]";
+        string modifiedIdentifier = Regex.Replace(identifier, pattern, "CTRL");
+        char[] charArray = modifiedIdentifier.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (charArray[i] == ' ')
+                charArray[i] = '_';
+
+            if (charArray[i] == '-' && i + 1 < charArray.Length)
+            {
+                charArray[i + 1] = char.ToUpper(charArray[i + 1]);
+            }
+        }
+
+        string modifiedIdentifierFinal = new(charArray);
+        return modifiedIdentifierFinal;
     }
 }
