6c61fc43841d42fed6e2283a1337c2656d1c1c9a
BlockSC <79803546+BlockSC@users.noreply.github.com>
2023-08-09 17:07:30 +0200

    final solution

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -5,6 +5,7 @@ public static class ReverseString
     public static string Reverse(string input)
     {
         char[] charArray = input.ToCharArray();
+        
         Array.Reverse(charArray);
         return new string (charArray);
     }
