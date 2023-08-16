238277a89a8f21ec7856f0b91ee67ed5666627c6
GSC <gsc.code@gmail.com>
2023-08-16 07:45:45 +0200

    final solution

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -22,6 +22,6 @@ public static class Bob
             return "Sure.";
         // Default response for all other cases
         else
-            return "Whatever.";
+            return "Whatever."; 
     }
 }
