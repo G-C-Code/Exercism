954b16fe810e9b8e1f87a0eb2007ebbc6038063b
GSC <gsc.code@gmail.com>
2023-10-27 07:19:06 +0200

    final solution

---
+++ csharp/grains/Grains.cs
@@ -0,0 +1,10 @@
+using System;
+
+public static class Grains
+{
+    public static ulong Square(int n) => n > 64 || n < 1
+    ? throw new ArgumentOutOfRangeException()
+    : 1 * (ulong)Math.Pow(2, n - 1);
+
+    public static ulong Total() => (ulong)Math.Pow(2, 64) - 1;
+}
