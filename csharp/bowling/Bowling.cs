ecee3fe171134b17eb6ed0f96ecf652f16371d20
GSC <gsc.code@gmail.com>
2023-11-09 07:45:35 +0100

    final solution

--- csharp/bowling/Bowling.cs
+++ csharp/bowling/Bowling.cs
@@ -5,36 +5,34 @@ public class BowlingGame
 {
     public List<Frame> Frames = new();
     public int FrameCount = 1;
-    public bool FirstThrow = true;
+    public bool SecondThrow;
     public bool FillBall;
-    public int FirstRoll { get; set; }
-    public int SecondRoll { get; set; }
-    public int ThirdRoll { get; set; }
+    public int FirstRoll = 0;
+    public int SecondRoll = 0;
+    public int ThirdRoll = 0;
 
     public void Roll(int pins) 
     {
         if (pins > 10 || pins < 0 || FrameCount > 10)
             throw new ArgumentException();
 
-
         if (FrameCount < 10) //First frames
         {
-            if (FirstThrow)
+            if (!SecondThrow)
             {
                 FirstRoll = pins;
                 
                 if (pins == 10) //Strike
                 {
+                    FrameCount++;
                     Frame frame = new(FirstRoll, 0);
                     Frames.Add(frame);
-
                     FirstRoll = 0;
                     SecondRoll = 0;
-                    FrameCount++;
                 }
                 else
                 {
-                    FirstThrow = false;
+                    SecondThrow = true;
                 }
             }
             else
@@ -44,21 +42,20 @@ public class BowlingGame
                 if (FirstRoll + SecondRoll > 10)
                     throw new ArgumentException();
 
+                FrameCount++;
                 Frame frame = new(FirstRoll, SecondRoll);
                 Frames.Add(frame);
-
-                FirstThrow = true;
+                SecondThrow = false;
                 FirstRoll = 0;
                 SecondRoll = 0;
-                FrameCount++;
             }
         }
-        else
+        else //Last frame
         {
-            if (FirstThrow)
+            if (!SecondThrow)
             {
                 FirstRoll = pins;
-                FirstThrow = false;
+                SecondThrow = true;
             }
             else
             {
@@ -69,26 +66,30 @@ public class BowlingGame
                     if (FirstRoll != 10 && FirstRoll + SecondRoll > 10)
                         throw new ArgumentException();
 
-                    if (FirstRoll == 10 || FirstRoll + SecondRoll == 10)
+                    if (FirstRoll == 10 || FirstRoll != 10 && FirstRoll + SecondRoll == 10)
+                    {
                         FillBall = true;
-
-                    if (FirstRoll + SecondRoll < 10)
+                    }
+                    else if (FirstRoll + SecondRoll < 10)
+                    {
                         FrameCount++;
-
-                    Frame frame = new(FirstRoll, SecondRoll);
-                    Frames.Add(frame);
-
-                    FirstRoll = 0;
-                    SecondRoll = 0;
+                        Frame frame = new(FirstRoll, SecondRoll);
+                        Frames.Add(frame);
+                    }
                 }
-                else
+                else if (FillBall)
                 {
                     ThirdRoll = pins;
+
+                    if (FirstRoll == 10 && SecondRoll != 10 && SecondRoll + ThirdRoll > 10)
+                        throw new ArgumentException();
+
                     FrameCount++;
+                    Frame frame = new(FirstRoll, SecondRoll);
+                    Frames.Add(frame);
                 }
             }
         }
-
     }
 
     public int? Score()
@@ -103,38 +104,25 @@ public class BowlingGame
             if (i < 8)
             {
                 if (Frames[i].FirstRoll == 10 && Frames[i+1].FirstRoll == 10)
-                {
                     Frames[i].FirstRoll += Frames[i+1].FirstRoll + Frames[i+2].FirstRoll;
-                }
                 else if (Frames[i].FirstRoll == 10)
-                {
                     Frames[i].FirstRoll += Frames[i+1].FirstRoll + Frames[i+1].SecondRoll;
-                }
                 else if (Frames[i].FirstRoll + Frames[i].SecondRoll == 10)
-                {
                     Frames[i].FirstRoll += Frames[i+1].FirstRoll;
-                }
             }
             else if (i == 8)
             {
                 if (Frames[i].FirstRoll == 10)
-                {
                     Frames[i].FirstRoll += Frames[i+1].FirstRoll + Frames[i+1].SecondRoll;
-                }
                 else if (Frames[i].FirstRoll + Frames[i].SecondRoll == 10)
-                {
                     Frames[i].SecondRoll += Frames[i+1].FirstRoll;
-                }
             }
         }
 
-        if (FillBall)
-            score += ThirdRoll;
+        score += ThirdRoll;
 
         foreach (Frame f in Frames)
-        {
             score += f.FirstRoll + f.SecondRoll;
-        }
 
         return score;
     }
@@ -144,10 +132,5 @@ public class Frame
 {
     public int FirstRoll { get; set; }
     public int SecondRoll { get; set; }
-
-    public Frame (int firstRoll, int secondRoll)
-    {
-        FirstRoll = firstRoll;
-        SecondRoll = secondRoll;
-    }
+    public Frame (int firstRoll, int secondRoll) => (FirstRoll, SecondRoll) = (firstRoll, secondRoll);
 }
