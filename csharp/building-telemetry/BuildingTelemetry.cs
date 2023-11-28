e11dc30a999ddb75d3100d30a4af754af5b64e6c
GSC <gsc.code@gmail.com>
2023-11-28 13:16:14 +0100

    final solution

---
+++ csharp/building-telemetry/BuildingTelemetry.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class RemoteControlCar
+{
+    private int batteryPercentage = 100;
+    private int distanceDrivenInMeters = 0;
+    private List<string> sponsors = new();
+    private int latestSerialNum = 0;
+
+    public void Drive()
+    {
+        if (batteryPercentage > 0)
+        {
+            batteryPercentage -= 10;
+            distanceDrivenInMeters += 2;
+        }
+    }
+
+    public void SetSponsors(params string[] sponsors) => this.sponsors.AddRange(sponsors);
+
+    public string DisplaySponsor(int sponsorNum) => sponsors[sponsorNum];
+
+    public bool GetTelemetryData(ref int serialNum,
+        out int batteryPercentage, out int distanceDrivenInMeters)
+    {
+        if (latestSerialNum >= serialNum)
+        {
+            serialNum = latestSerialNum;
+            batteryPercentage = -1;
+            distanceDrivenInMeters = -1;
+            return false;
+        }
+        else
+        {
+            latestSerialNum = serialNum;
+            batteryPercentage = this.batteryPercentage;
+            distanceDrivenInMeters = this.distanceDrivenInMeters;
+            return true;
+        }
+    }
+
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+}
+
+public class TelemetryClient
+{
+    private RemoteControlCar car;
+
+    public TelemetryClient(RemoteControlCar car) => this.car = car;
+
+    public string GetBatteryUsagePerMeter(int serialNum) => !car.GetTelemetryData(ref serialNum,
+        out int batteryPercentage, out int distanceDrivenInMeters) || distanceDrivenInMeters == 0
+        ? "no data"
+        : $"usage-per-meter={(100-batteryPercentage)/distanceDrivenInMeters}";
+}
